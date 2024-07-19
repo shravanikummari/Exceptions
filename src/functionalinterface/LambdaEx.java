05021661b177ccf15382f6ba444c05506078490b
shravanikummari <shravanikummari1234@gmail.com>
2024-07-19 05:38:22 -0700

    LamdaEx task added

---
+++ src/functionalinterface/LambdaEx.java
@@ -0,0 +1,23 @@
+package functionalinterface;
+interface Shravani
+{
+    String name(String str);
+}
+interface Cooking
+{
+    void cook();
+}
+interface Multiply
+{
+    int Multiply(int a,int b);
+}
+public class LambdaEx {
+    public static void main(String[] args) {
+        Shravani S=(s) -> "Hi my name is shravani "+s;
+        Cooking C=() -> System.out.println(" shravani is cooking....");
+        Multiply M=(a,b)-> a*b;
+        System.out.println(S.name("chikki"));
+        C.cook();
+        M.Multiply(5,6);
+    }
+}
